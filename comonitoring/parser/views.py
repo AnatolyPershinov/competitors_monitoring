76527c87b9caed79354489def8610f93b9e707f1
Anatoly <anatoliypershinov@gmail.com>
2022-08-17 18:33:48 +0300

    add requirements

--- comonitoring/parser/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-    # main page - display price changes from database
-    return HttpResponse()
-
-# Create your views here.
